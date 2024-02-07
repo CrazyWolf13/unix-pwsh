3f2440da17bf587ae9b1b25763a6c63a8dc60e3c
Tobias <tobias.meier109@gmail.com>
2024-02-07 21:17:37 +0100

    added

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,195 @@
+
+#34de4b3d-13a8-4540-b76d-b9e8d3851756 PowerToys CommandNotFound module
+
+Import-Module "C:\Users\tobia\AppData\Local\PowerToys\WinUI3Apps\..\WinGetCommandNotFound.psd1"
+#34de4b3d-13a8-4540-b76d-b9e8d3851756
+Import-Module -Name Terminal-Icons
+
+Write-Host ""
+Write-Host "Welcome Tobias ⚡" -ForegroundColor DarkCyan
+Write-Host ""
+
+
+# Black, Blue, Cyan, DarkBlue, DarkCyan, DarkGray, DarkGreen, DarkMagenta, DarkRed, DarkYellow, Gray, Green, Magenta, Red, White, Yellow.
+
+
+function gitpush {
+    param (
+        [string]$CommitMessage
+    )
+
+    if (-not $CommitMessage) {
+        Write-Host "Please provide a commit message."
+        return
+    }
+
+    git add .
+    git commit -m "$CommitMessage"
+    git push
+}
+
+
+
+
+# Find out if the current user identity is elevated (has admin rights)
+$identity = [Security.Principal.WindowsIdentity]::GetCurrent()
+$principal = New-Object Security.Principal.WindowsPrincipal $identity
+$isAdmin = $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+
+# If so and the current host is a command line, then change to red color 
+# as warning to user that they are operating in an elevated context
+# Useful shortcuts for traversing directories
+function cd... { Set-Location ..\.. }
+function cd.... { Set-Location ..\..\.. }
+
+# Compute file hashes - useful for checking successful downloads 
+function md5 { Get-FileHash -Algorithm MD5 $args }
+function sha1 { Get-FileHash -Algorithm SHA1 $args }
+function sha256 { Get-FileHash -Algorithm SHA256 $args }
+
+# Quick shortcut to start notepad
+function n { notepad $args }
+function vs { code $args }
+function expl { explorer . }
+
+# Set up command prompt and window title. Use UNIX-style convention for identifying 
+# whether user is elevated (root) or not. Window title shows current version of PowerShell
+# and appends [ADMIN] if appropriate for easy taskbar identification
+function prompt { 
+    if ($isAdmin) {
+        "[" + (Get-Location) + "] # " 
+    } else {
+        "[" + (Get-Location) + "] $ "
+    }
+}
+
+$Host.UI.RawUI.WindowTitle = "PowerShell {0}" -f $PSVersionTable.PSVersion.ToString()
+if ($isAdmin) {
+    $Host.UI.RawUI.WindowTitle += " [ADMIN]"
+}
+
+# Does the the rough equivalent of dir /s /b. For example, dirs *.png is dir /s /b *.png
+function dirs {
+    if ($args.Count -gt 0) {
+        Get-ChildItem -Recurse -Include "$args" | Foreach-Object FullName
+    } else {
+        Get-ChildItem -Recurse | Foreach-Object FullName
+    }
+}
+
+# Simple function to start a new elevated process. If arguments are supplied then 
+# a single command is started with admin rights; if not then a new admin instance
+# of PowerShell is started.
+function admin {
+    if ($args.Count -gt 0) {   
+        $argList = "& '" + $args + "'"
+        Start-Process "wt.exe" -Verb runAs -ArgumentList $argList
+    } else {
+        Start-Process "wt.exe" -Verb runAs
+    }
+}
+
+# Set UNIX-like aliases for the admin command, so sudo <command> will run the command
+# with elevated rights. 
+Set-Alias -Name su -Value admin
+Set-Alias -Name sudo -Value admin
+
+
+# Make it easy to edit this profile once it's installed
+function Edit-Profile {
+    if ($host.Name -match "ise") {
+        $psISE.CurrentPowerShellTab.Files.Add($profile.CurrentUserAllHosts)
+    } else {
+        code $profile
+    }
+}
+
+# We don't need these any more; they were just temporary variables to get to $isAdmin. 
+# Delete them to prevent cluttering up the user profile. 
+Remove-Variable identity
+Remove-Variable principal
+
+Function Test-CommandExists {
+    Param ($command)
+    $oldPreference = $ErrorActionPreference
+    $ErrorActionPreference = 'SilentlyContinue'
+    try { if (Get-Command $command) { RETURN $true } }
+    Catch { Write-Host "$command does not exist"; RETURN $false }
+    Finally { $ErrorActionPreference = $oldPreference }
+} 
+#
+# Aliases
+#
+# If your favorite editor is not here, add an elseif and ensure that the directory it is installed in exists in your $env:Path
+#
+if (Test-CommandExists code) {
+    $EDITOR='code'
+}  elseif (Test-CommandExists notepad) {
+    $EDITOR='notepad'
+} elseif (Test-CommandExists notepad++) {
+    $EDITOR='notepad++'
+} 
+Set-Alias -Name vim -Value $EDITOR
+
+
+function ll { Get-ChildItem -Path $pwd -File }
+
+function Get-PubIP {
+    (Invoke-WebRequest http://ifconfig.me/ip ).Content
+}
+
+
+function unzip ($file) {
+    $fullPath = Join-Path -Path $pwd -ChildPath $file
+    if (Test-Path $fullPath) {
+        Write-Output "Extracting $file to $pwd"
+        Expand-Archive -Path $fullPath -DestinationPath $pwd
+    } else {
+        Write-Output "File $file does not exist in the current directory"
+    }
+}
+
+function sync-profile {
+    clear-host
+    @(
+        $Profile.AllUsersAllHosts,
+        $Profile.AllUsersCurrentHost,
+        $Profile.CurrentUserAllHosts,
+        $Profile.CurrentUserCurrentHost
+    ) | ForEach-Object {
+        if(Test-Path $_){
+            Write-Verbose "Running $_"
+            . $_
+        }
+    }
+}
+Set-Alias -Name reload -Value sync-profile
+
+function grep($regex, $dir) {
+    if ( $dir ) {
+        Get-ChildItem $dir | select-string $regex
+        return
+    }
+    $input | select-string $regex
+}
+function touch($file) {
+    "" | Out-File $file -Encoding ASCII
+}
+function lsblk {
+    get-volume
+}
+function sed($file, $find, $replace) {
+    (Get-Content $file).replace("$find", $replace) | Set-Content $file
+}
+function which($name) {
+    Get-Command $name | Select-Object -ExpandProperty Definition
+}
+function pkill($name) {
+    Get-Process $name -ErrorAction SilentlyContinue | Stop-Process
+}
+function pgrep($name) {
+    Get-Process $name
+}
+
+$themePath = Split-Path $PROFILE -Parent | Join-Path -ChildPath 'montys.omp.json'
+oh-my-posh init pwsh --config $themePath | Invoke-Expression
