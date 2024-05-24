23e672d34b15039109f63e8911e818f6e51f06d7
Tobias <tobias.meier109@gmail.com>
2024-05-24 20:37:59 +0200

    update

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -179,8 +179,8 @@ function Initialize-Modules {
 
 function Test-Applications {
     if (Test-CommandExists code) {
-        Set-ConfigValue -Key "VSCode_installed" -Value "True"
         Write-Host "Successfully detected Visual Studio Code installation. " -ForegroundColor Green
+        Set-ConfigValue -Key "VSCode_installed" -Value "True"
     } else {
         $installVSCode = Read-Host "Do you want to install Visual Studio Code? (Y/N)"
         if ($installVSCode -eq 'Y' -or $installVSCode -eq 'y') {
@@ -188,9 +188,7 @@ function Test-Applications {
         } else {
             Write-Host "Visual Studio Code installation skipped." -ForegroundColor Yellow
         }
-    }
-    Set-ConfigValue -Key "VSCode_installed" -Value "True"
-    
+    }    
     if (Test-CommandExists oh-my-posh) {
         Write-Host "Successfully detected Oh-My-Posh installation." -ForegroundColor Green
         Set-ConfigValue -Key "OhMyPosh_installed" -Value "True"
