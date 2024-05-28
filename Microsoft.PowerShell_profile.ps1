d9fe2df6e665c53fc071b518ae222fa1816dcb0c
Tobias <96661824+CrazyWolf13@users.noreply.github.com>
2024-05-28 17:03:33 +0200

    revert revert ptw

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -293,11 +293,10 @@ function tail {
     Get-Content $Path -Tail $n
 }
 
-
-$WastebinServerUrl=[System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String("aHR0cHM6Ly9iaW4uY3Jhenl3b2xmLmRldg=="))
-$DefaultExpirationTime = 3600  # Default expiration time: 1 hour (in seconds)
-$DefaultBurnAfterReading = $false  # Default value for burn after reading setting
 function ptw {
+    $WastebinServerUrl=[System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String("aHR0cHM6Ly9iaW4uY3Jhenl3b2xmLmRldg=="))
+    $DefaultExpirationTime = 3600  # Default expiration time: 1 hour (in seconds)
+    $DefaultBurnAfterReading = $false  # Default value for burn after reading setting
     [CmdletBinding()]
     param (
         [Parameter(Position=0)]
@@ -341,6 +340,9 @@ function ptw {
 }
 
 function pptw {
+    $WastebinServerUrl=[System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String("aHR0cHM6Ly9iaW4uY3Jhenl3b2xmLmRldg=="))
+    $DefaultExpirationTime = 3600  # Default expiration time: 1 hour (in seconds)
+    $DefaultBurnAfterReading = $false  # Default value for burn after reading setting
     [CmdletBinding()]
     param (
         [Parameter(ValueFromPipeline=$true)]
