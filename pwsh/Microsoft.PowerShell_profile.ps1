6abd1ea693bb7665b8befc127e1ce91a677ae3b4
Tobias <96661824+CrazyWolf13@users.noreply.github.com>
2024-05-30 18:50:52 +0200

    Update Microsoft.PowerShell_profile.ps1

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -143,13 +143,6 @@ Function Test-CommandExists {
 . Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/CrazyWolf13/home-configs/main/pwsh/custom_functions.ps1" -UseBasicParsing).Content
 . Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/CrazyWolf13/home-configs/main/pwsh/functions.ps1" -UseBasicParsing).Content
 
-
-
-# TEMP:
-#Update all my Configs to the new subfolder:
-Set-Content -Path $PROFILE -Value 'iex (iwr "https://raw.githubusercontent.com/CrazyWolf13/home-configs/main/pwsh/Microsoft.PowerShell_profile.ps1").Content'
-
-
 # -------------
 # Run section
 
