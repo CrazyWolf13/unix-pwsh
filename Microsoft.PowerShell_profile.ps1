b38f211d8343ea73a0d9fdb77e9e07832396a225
Tobias Meier <tobias.meier109@gmail.com>
2024-05-31 14:48:17 +0200

    dynamic font installer

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -36,7 +36,8 @@ function Initialize-DevEnv {
         . Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/$githubUser/unix-pwsh/main/pwsh_helper.ps1" -UseBasicParsing).Content
         Test-ohmyposh 
         }
-    if ($($font + "_installed") -ne "True") {
+    $font_installed = "$($font)_installed"
+    if ($font_installed -ne "True") {
         Write-Host "⚡ Invoking Helper-Script" -ForegroundColor Yellow
         Write-Host "Trying to to check if Nerd-Fonts are installed" -ForegroundColor Yellow
         . Invoke-Expression (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/$githubUser/unix-pwsh/main/pwsh_helper.ps1" -UseBasicParsing).Content
