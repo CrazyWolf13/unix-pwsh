23852d7675cdb6b84b8de5aab35379b4316c1375
Tobias Meier <tobias.meier109@gmail.com>
2024-05-31 07:14:30 +0200

    remove personal assets

--- linux/post-install.sh
+++
@@ -1,9 +0,0 @@
-sudo apt update
-sudo add-apt-repository ppa:zhangsongcui3371/fastfetch -y
-sudo apt update
-sudo apt upgrade -y
-sudo apt install nala btop screen -y
-clear
-echo "IP is: $(hostname -I | awk '{print $1}')"
-echo "Hostname is: $(hostname)"
-echo "Done!"
