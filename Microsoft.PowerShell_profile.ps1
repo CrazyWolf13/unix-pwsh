272aae4cbc686f763b4275d14dbe86affcb0f979
Tobias <96661824+CrazyWolf13@users.noreply.github.com>
2024-09-06 09:43:33 +0200

    fix vars

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,7 @@
 $githubUser = "CrazyWolf13" # Change this here if you forked the repository.
 $name= "User" # Change this to your name.
-$OhMyPoshConfig = "https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/$OhMyPoshConfigFileName" # URL of the OhMyPosh config file, make sure to use the last part of the raw lik, (stands for the filename) in the variable on the line below
 $OhMyPoshConfigFileName = "montys.omp.json" # Filename of the OhMyPosh config file
+$OhMyPoshConfig = "https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/$OhMyPoshConfigFileName" # URL of the OhMyPosh config file, make sure to use the last part of the raw lik, (stands for the filename) in the variable on the line below
 
 # -----------------------------------------------------------------------------
 
